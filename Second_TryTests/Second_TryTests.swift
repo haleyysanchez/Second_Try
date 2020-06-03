dc374a5b5e55edf3fb9238be74b43a21d6e7f851
haleyysanchez <haleymsanchez910@gmail.com>
2020-06-03 10:54:48 -0400

    Initial Commit

---
+++ Second_TryTests/Second_TryTests.swift
@@ -0,0 +1,34 @@
+//
+//  Second_TryTests.swift
+//  Second_TryTests
+//
+//  Created by Haley Sanchez on 6/3/20.
+//  Copyright © 2020 Haley Sanchez. All rights reserved.
+//
+
+import XCTest
+@testable import Second_Try
+
+class Second_TryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
